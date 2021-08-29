9cff3833a535d8c71c425989dccc928cb627ee0b
Guilherme_gra <49792439+Guilherme-Rodrigues-Almeida@users.noreply.github.com>
2021-08-29 17:01:23 -0300

    Aula 3

--- Aula 3/aula3.py
+++ Aula 3/aula3.py
@@ -2,7 +2,7 @@
 n1 = int(input("Digite um número: "))
 n2 = int(input("Digite mais um número: "))
 soma = n1 + n2
-print("A soma entre {} e {} vale = {}".format(n1, n2, soma))
+print(f"A soma entre {n1} e {n2} vale = {soma}")
 print(type(soma))
 print("----------")
 print("----------")
