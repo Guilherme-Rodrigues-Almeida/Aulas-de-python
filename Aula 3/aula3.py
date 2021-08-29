f6106b4a977ecf0cc9ad06859d391807edae4a7a
Guilherme_gra <49792439+Guilherme-Rodrigues-Almeida@users.noreply.github.com>
2021-08-29 16:40:15 -0300

    Aula 3

--- Aula 3/aula3.py
+++ Aula 3/aula3.py
@@ -1,7 +1,9 @@
 #tipos primitivos
 n1 = int(input("Digite um número: "))
 n2 = int(input("Digite mais um número: "))
-s = n1 + n2
-print("A soma vale = {}".format(s))
-
+soma = n1 + n2
+print("A soma entre {} e {} vale = {}".format(n1, n2, soma))
+print(type(soma))
+print("----------")
+print("----------")
 #int = inteiros, float= numeros reias ou de ponto flutuante, bool=valores logicos ou booleanos , str= strings ou caracteres
