ede5150706409b1c7b9ad0febe5dd2808d5c387d
Guilherme_gra <49792439+Guilherme-Rodrigues-Almeida@users.noreply.github.com>
2021-08-29 16:26:43 -0300

    python classes

---
+++ Aula 3/aula3.py
@@ -0,0 +1,7 @@
+#tipos primitivos
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite mais um número: "))
+s = n1 + n2
+print("A soma vale =", s, "!")
+
+#int = inteiros, float= numeros reias ou de ponto flutuante, bool=valores logicos ou booleanos , str= strings ou caracteres
