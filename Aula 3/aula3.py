375eff516d16d24356c32a4389b2f5615bdeef10
Guilherme_gra <49792439+Guilherme-Rodrigues-Almeida@users.noreply.github.com>
2021-08-29 16:31:13 -0300

    Aula 3

--- Aula 3/aula3.py
+++ Aula 3/aula3.py
@@ -2,6 +2,6 @@
 n1 = int(input("Digite um número: "))
 n2 = int(input("Digite mais um número: "))
 s = n1 + n2
-print("A soma vale =", s, "!")
+print("A soma vale = {}".format(s))
 
 #int = inteiros, float= numeros reias ou de ponto flutuante, bool=valores logicos ou booleanos , str= strings ou caracteres
