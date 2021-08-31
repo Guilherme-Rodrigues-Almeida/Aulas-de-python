7a60429ca43940ccafcadfc48feb3f6bcd77ffa0
Guilherme_gra <49792439+Guilherme-Rodrigues-Almeida@users.noreply.github.com>
2021-08-31 01:12:58 -0300

    Done desafio 011

---
+++ Aula 4/desafio011.py
@@ -0,0 +1,5 @@
+print("")
+largura = int(input("Digite a largura da parede: ")) 
+altura = int(input("Digite a altura da parede: "))
+print("")
+print(f"A área da parede é: {largura *  altura} m²;\nSerá necessário usar: {(largura * altura) / 2} litros de tinta")
