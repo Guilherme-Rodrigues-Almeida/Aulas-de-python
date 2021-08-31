f2d775910042619ea09586eb9a250f20eebb440e
Guilherme_gra <49792439+Guilherme-Rodrigues-Almeida@users.noreply.github.com>
2021-08-31 01:01:00 -0300

    done dasafio 009

---
+++ Aula 4/desafio009.py
@@ -0,0 +1,4 @@
+print("")
+num = int(input("Digite um número: "))
+print("")
+print(f"Mostrando a tabuada do {num}\n{num} x 0 = {num * 0}\n{num} x 1 = {num * 1}\n{num} x 2 = {num * 2}\n{num} x 3 = {num * 3}\n{num} x 4 = {num * 4}\n{num} x 5 = {num * 5}\n{num} x 6 = {num * 6}\n{num} x 7 = {num * 7}\n{num} x 8 = {num * 8}\n{num} x 9 = {num * 9}\n{num} x 10 = {num * 10}\n")
