5aced9f9e86474fbd1a062d45468ef3c5416fdef
Guilherme_gra <49792439+Guilherme-Rodrigues-Almeida@users.noreply.github.com>
2021-08-31 01:33:52 -0300

    Done

--- Aula 4/aula04.py
+++ Aula 4/aula04.py
@@ -20,8 +20,8 @@ print("Prazer em te conhecer {:=^20}!".format(nome))
 print("")
 print("")
 
-n1 = int(input("Um valor: "))
-n2= int(input("Segundo valor: "))
+n1 = float(input("Um valor: "))
+n2= float(input("Segundo valor: "))
 
-print(f"A soma vale: {n1 + n2}, \no produto é: {n1 * n2}, \na divisão é: {n1 / n2}", end = ", ")
-print(f"\ndivisão inteira é: {n1 // n2} \ne a potência é: {n1 ** n2}")
+print(f"A soma vale: {n1 + n2:.3f}, \no produto é: {n1 * n2:.3f}, \na divisão é: {n1 / n2:.3f}", end = ", ")
+print(f"\ndivisão inteira é: {n1 // n2:.3f} \ne a potência é: {n1 ** n2:.3f}")
