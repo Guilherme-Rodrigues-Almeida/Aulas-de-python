96ba8b72486860b1a996fbe9d10a1c308a089ff0
Guilherme_gra <49792439+Guilherme-Rodrigues-Almeida@users.noreply.github.com>
2021-08-31 01:06:39 -0300

    done dasafio 010

---
+++ Aula 4/desafio010.py
@@ -0,0 +1,4 @@
+print("")
+dinheiro = int(input("Quanto dinherio você tem na conta (em reais): "))
+print("")
+print(f"Você possui: {dinheiro} reais;\nVocê consegue comprar {dinheiro * 3.27} Dólares")
