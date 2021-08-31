bbf5b6be302c80da77c777c0087b3ce3e8ff625a
Guilherme_gra <49792439+Guilherme-Rodrigues-Almeida@users.noreply.github.com>
2021-08-31 00:24:35 -0300

    Aula 04 finalizada

--- Aula 4/aula04.py
+++ Aula 4/aula04.py
@@ -1,1 +1,27 @@
+# Operadores aritiméticos
 # ** = potencia; // = divisão inteira; % = resto da divisão
+
+#ordem de precedência: 1º (); 2º **;  3º *,/,//,% ;  4º +,-;
+
+#Raiz quadrada de um número(n) = n**0.5 ou n**(1/2)
+#Raiz cúbica n**(1/3)
+
+n = 81
+print(n**(1/2))
+print(n**(1/3))
+
+print("ola" * 20)
+print("")
+print("")
+
+nome = input("Qual o seu nome?: ")
+print("Prazer em te conhecer {:=^20}!".format(nome))
+
+print("")
+print("")
+
+n1 = int(input("Um valor: "))
+n2= int(input("Segundo valor: "))
+
+print(f"A soma vale: {n1 + n2}, \no produto é: {n1 * n2}, \na divisão é: {n1 / n2}", end = ", ")
+print(f"\ndivisão inteira é: {n1 // n2} \ne a potência é: {n1 ** n2}")
