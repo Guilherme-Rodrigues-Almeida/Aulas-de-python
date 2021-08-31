618689ae379fd27a28e5268f3ffd865bb47bf03d
Guilherme_gra <49792439+Guilherme-Rodrigues-Almeida@users.noreply.github.com>
2021-08-31 00:51:40 -0300

    done dasafio 008

---
+++ Aula 4/desafio008.py
@@ -0,0 +1,4 @@
+print("")
+valor_em_metros = int(input("Digite o valor em metros: "))
+print("")
+print(f"O valor em centímetros é: {valor_em_metros * 100} cm;\nO valor em milímetros: {valor_em_metros * 1000} mm")
