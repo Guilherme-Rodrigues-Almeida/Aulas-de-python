7065e238240b46bf6712a50a6f94df6477d08765
Guilherme_gra <49792439+Guilherme-Rodrigues-Almeida@users.noreply.github.com>
2021-08-31 00:41:40 -0300

    done dasafio 007

---
+++ Aula 4/desafio007.py
@@ -0,0 +1,5 @@
+print("")
+nota1 = int(input("Digite a primeira nota: "))
+nota2= int(input("Digite a segunda nota: "))
+print("")
+print(f"Primeira nota: {nota1};\nSegunda nota: {nota2};\nA média das duas notas é: {(nota1 + nota2)/2}.")
